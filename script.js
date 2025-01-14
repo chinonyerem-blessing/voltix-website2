413bf965e1287470bcd71ded85d16963ac606c98
chinonyerem-blessing <chinonyeremblessing95@gmail.com>
2025-01-14 11:02:21 +0100

    nav work

--- script.js
+++ script.js
@@ -159,3 +159,16 @@ window.addEventListener('scroll', () => {
     }
 });
 
+const navLinks = document.querySelectorAll("nav a");
+navLinks.forEach(link => {
+
+    link.addEventListener("click", event => {
+        event.preventDefault();
+
+        const sectionId = link.getAttribute("href");
+
+        const section = document.querySelector(sectionId);
+
+        section.scrollIntoView({ behaviour: "smooth" })
+    });
+});
